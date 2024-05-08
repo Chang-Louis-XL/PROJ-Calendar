394973b4018f0d51cee91c84f6aa39e9c8ea2cc5
Chang Louis <louisvictory0226@gmail.com>
2024-05-08 20:57:39 +0800

    re

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,17 +8,78 @@
   <link rel="shortcut icon" href="#">
   <title>萬年曆作業</title>
   <style>
-    table {
-      border-collapse: collapse;
-      border: 3px double blue;
+    /* 以下為框架樣式 */
+    * {
+      box-sizing: border-box;
     }
 
-    td {
-      padding: 5px 10px;
-      border: 1px solid lightgreen;
+    .container {
+      width: 80%;
+      margin: auto;
+      /* position: relative;
+      display: flex;
+      justify-content: space-between; */
+      border: 1px solid transparent;
+      border-radius: 60px;
+      padding: 60px;
+      background-color: #FEFEFE;
+      box-shadow: 9px 11px 11px 3px black;
+
+      /* position: relative;
+      display: flex;
+      justify-content: space-between;
+      margin: auto;
+      margin-top: 50px;
+      border: 2px solid black;
+      border-radius: 25px;
+      padding: 15px;
+      position: relative; */
+    }
+
+
+    .row {
+      margin: auto;
+      position: relative;
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .leftbox {
+      margin: 0;
+      /* border: 3px dotted #7F989E; */
+      border-radius: 60px;
+      box-shadow: 3px 4px 12px 0px #7A7567;
+      background: url(https://images.unsplash.com/photo-1520853538280-53740d2bd110?ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80);
+      background-repeat: no-repeat;
+      background-size: cover;
+      width: 49%;
+      height: 650px;
+
+      /* position: absolute;
+      width: 49%;
+      height: 750px;
+      margin-right: 20px;
+      border: 2px solid black;
+      background-color: lightyellow; */
+    }
+
+    .rightbox {
+      margin: 0px;
+      padding-top: 50px;
+      border: 1px solid transparent;
+      width: 49%;
+      height: 650px;
+
+      /* position: absolute;
+      right: 10px;
+      width: 49%;
+      height: 750px;
+      border: 2px solid black;
+      background-color: lightgreen; */
 
     }
 
+    /* 以下為萬年曆CSS */
     .block-table {
       width: 380px;
       display: flex;
@@ -63,78 +125,9 @@
     }
 
     .othermday{
-      
-    }
-
-    /* 以下為框架樣式 */
-    * {
-      box-sizing: border-box;
-    }
-
-    .container {
-      width: 80%;
-      margin: auto;
-      /* position: relative;
-      display: flex;
-      justify-content: space-between; */
-      border: 1px solid transparent;
-      border-radius: 60px;
-      padding: 60px;
-      background-color: #FEFEFE;
-      box-shadow: 9px 11px 11px 3px black;
-
-      /* position: relative;
-      display: flex;
-      justify-content: space-between;
-      margin: auto;
-      margin-top: 50px;
-      border: 2px solid black;
-      border-radius: 25px;
-      padding: 15px;
-      position: relative; */
-    }
-
-    
-    .row {
-      margin: auto;
-      position: relative;
-      display: flex;
-      justify-content: space-between;
-    }
-
-    .leftbox {
-      margin: 0;
-      /* border: 3px dotted #7F989E; */
-      border-radius: 60px;
-      box-shadow: 3px 4px 12px 0px #7A7567;
-      background: url(https://images.unsplash.com/photo-1520853538280-53740d2bd110?ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80);
-      background-repeat: no-repeat;
-      background-size: cover;
-      width: 49%;
-      height: 650px;
-
-      /* position: absolute;
-      width: 49%;
-      height: 750px;
-      margin-right: 20px;
-      border: 2px solid black;
-      background-color: lightyellow; */
-    }
-
-    .rightbox {
-      margin: 0px;
-      padding-top: 50px;
-      border: 1px solid transparent;
-      width: 49%;
-      height: 650px;
-
-      /* position: absolute;
-      right: 10px;
-      width: 49%;
-      height: 750px;
-      border: 2px solid black;
-      background-color: lightgreen; */
-
+    font-weight: bold;       
+    color: gray;  
+    opacity: 0.3;       
     }
   </style>
 </head>
@@ -146,49 +139,55 @@
   <br>
   <div class="container">
     <div class="row">
-    <div class="leftbox">
-    </div>
-    <div class="rightbox">
-      <div class="calendar">
-        <form action="?" method="get">
-          <label for="month"></label>
-          <input type="number" name='month' value="<?= date("m"); ?>">
-          <input type="submit" value="送出">
-        </form>
-        <?php
-        $month = $_GET['month'] ?? date('m');
-        $month = $_GET['year'] ?? date('Y');
-        $year = date("Y");
-        $m=
-        // echo "年" . $year;
-        // echo "<BR>";
-        // echo "月份:" . $month;
-        // echo "<br>";
-        $firstDay = strtotime(date("Y-$month-1"));
+      <div class="leftbox">
+      </div>
+      <div class="rightbox">
+        <?php   
+        $month = $_GET['month'] ?? date("m");
+        $year = $_GET['year'] ?? date("Y");
+        $firstDay = strtotime(date("$year-$month-1"));
         $firstWeekStartDay = date("w", $firstDay);
-        // echo "第一周的開始是第" . $firstWeekStartDay . "日";
         $days = date("t", $firstDay);
         $lastDay = strtotime(date("Y-$month-$days"));
-        // echo "<br>";
-        // echo "最後一天是" . date("Y-m-d", $lastDay);
-        $birthday = '1974-4-1';
+
         $days = [];
         for ($i = 0; $i < 42; $i++) {
           $diff = $i - $firstWeekStartDay;
           $days[] = date("Y-m-d", strtotime("$diff days", $firstDay));
         }
-        if ($month + 1 > 12) {
-        }
+
         if ($month - 1 < 1) {
+          $prev = 12;
+          $prev_year = $year - 1;
+        } else {
+          $prev = $month - 1;
+          $prev_year = $year;
         }
+
+        if ($month + 1 > 12) {
+          $next = 1;
+          $next_year = $year + 1;
+
+        } else {
+          $next = $month + 1;
+          $next_year = $year;
+        }
+
         ?>
         <div style="width:384px;">
-          <!-- 傳至calendar.php?month；<=($month-1);?>代表echo出這個值；style=為往左 -->
-          <a href="calendar.php?month=<?= ($month - 1); ?>" style="float:left">上一個月</a>
-          <a href="calendar.php?month=<?= ($month + 1); ?>" style="float:right">下一個月</a>
+          <div class="nav" style="text-align: center;">
+            <?= $year; ?>年 <?= $month; ?>月
+          </div>
+          <div class="nav" style="text-align: left;">
+            <a href="index.php?year=<?= $prev_year; ?>&month=<?= $prev; ?>">上一個月</a>
+          </div>
+          <div class="nav" style="text-align: right;">
+            <a href="index.php?year=<?= $next_year; ?>&month=<?= $next; ?>">下一個月</a>
+          </div>
         </div>
-        <div style='clear:both'></div>
+
         <?php
+
         echo "<div class='block-table'>";
         echo "<div class='item-header'>日</div>";
         echo "<div class='item-header'>一</div>";
@@ -201,20 +200,21 @@
           $format = explode("-", $day)[2];
           $w = date("w", strtotime($day));
           $m = date("m", strtotime($day));
-          if ($month!=$m){
-            echo "<div class= 'othermday'>$format</div>";
-          }else if($w == 0 || $w == 6) {
+          if($month!=$m){
+            echo "<div class='item othermday'>$format</div>";
+          } else if ($w == 0 || $w == 6) {
             echo "<div class='item holiday'>$format</div>";
           } else {
+
             echo "<div class='item'>";
             echo "<div class='date'>$format</div>";
             echo "</div>";
           }
         }
         echo "</div>";
+
         ?>
       </div>
-    </div>
     </div>
   </div>
 
